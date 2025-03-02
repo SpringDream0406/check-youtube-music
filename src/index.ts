f7b60c3f0aaa797c6b74456a3fcda290eb4b856f
SpringDream0406 <springdream0406@gmail.com>
2025-03-02 20:32:23 +0900

    log 변경/추가

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import cron from "node-cron";
 import { checkAllMusicData } from "./checkYoutubeVideosStatus";
 import { sendErrorEmail } from "./sendEmail";
 
-const now = new Date();
-console.log(`checkMusicData Start ${now.toLocaleString()}`);
+console.log("music-check Start", new Date().toLocaleString());
 
 // 매일 15시에 실행
 cron.schedule("0 15 * * *", async () => {
+  console.log("checking", new Date().toLocaleString());
+
   const result = await checkAllMusicData();
 
   // 이상 있는 데이터 있을 경우 email 보내기
