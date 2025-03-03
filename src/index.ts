ea570c03c2da49fb5e99d92decf00a16d92db93c
SpringDream0406 <springdream0406@gmail.com>
2025-03-03 20:42:57 +0900

    되돌림

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ console.log("music-check Start", new Date().toLocaleString());
 cron.schedule("0 15 * * *", async () => {
   console.log("checking", new Date().toLocaleString());
 
-  // const result = await checkAllMusicData();
+  const result = await checkAllMusicData();
 
   // 이상 있는 데이터 있을 경우 email 보내기
-  // if (result.length > 0) {
-  //   console.log(result);
-  //   sendErrorEmail(result);
-  // }
+  if (result.length > 0) {
+    console.log(result);
+    sendErrorEmail(result);
+  }
 });
