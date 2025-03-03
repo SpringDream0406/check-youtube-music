520a30051139838942616f0932c00a659d361251
SpringDream0406 <springdream0406@gmail.com>
2025-03-03 18:48:29 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ import { sendErrorEmail } from "./sendEmail";
 console.log("music-check Start", new Date().toLocaleString());
 
 // 매일 15시에 실행
-// cron.schedule("0 15 * * *", async () => {
-//   console.log("checking", new Date().toLocaleString());
+cron.schedule("0 15 * * *", async () => {
+  console.log("checking", new Date().toLocaleString());
 
-//   const result = await checkAllMusicData();
+  // const result = await checkAllMusicData();
 
-//   // 이상 있는 데이터 있을 경우 email 보내기
-//   if (result.length > 0) {
-//     console.log(result);
-//     sendErrorEmail(result);
-//   }
-// });
+  // 이상 있는 데이터 있을 경우 email 보내기
+  // if (result.length > 0) {
+  //   console.log(result);
+  //   sendErrorEmail(result);
+  // }
+});
