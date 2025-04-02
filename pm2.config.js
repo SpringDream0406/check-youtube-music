759746dd34a31c371f553c6c6d0b3435cbfbe29b
SpringDream0406 <springdream0406@gmail.com>
2025-04-02 20:15:30 +0900

    pm2명령어추가

--- pm2.config.js
+++ pm2.config.js
@@ -5,4 +5,10 @@ export const apps = [
     interpreter: "tsx",
     watch: true,
   },
+  {
+    name: "music-check-run",
+    script: "src/run.ts",
+    interpreter: "tsx",
+    watch: false, // 수동 실행이므로 watch는 비활성화
+  },
 ];
